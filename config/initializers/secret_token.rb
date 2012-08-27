a88bfb6cbaa0d0acce1adedb3ea229198be49f04
Brant Meier <pokemasterblm@gmail.com>
2012-08-27 12:11:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1ac70d61e3c33fcd67d054e15e898151def3b9e5b0f39733ad9645207d077b71979adeb270e6fe4ab8d65e0289472b46ed9eeae5710e7efeb02d500fdf29732a'
